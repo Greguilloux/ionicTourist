3b1a83b5ce66813b64ebd696b2107fc4ff2337a2
Grégoire Guilloux <@guilloux.gregoire>
2019-04-11 14:47:27 +0200

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { analyzeAndValidateNgModules, analyzeNgModules } from '@angular/compiler';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  
+
+}
